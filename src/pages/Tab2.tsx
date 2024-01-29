07b48d983fa74072b4557f15b45bb51dffd7e0aa
Logan Gray <lgray@proton.me>
2024-01-29 11:41:29 -0800

    Tab2 page refinement

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,14 +1,21 @@
 // src/pages/CheckboxPage.tsx
 import React from 'react';
-import { IonContent, IonPage, IonList, IonItem, IonLabel, IonCheckbox, IonRouterLink } from '@ionic/react';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonPage, IonList, IonItem, IonLabel, IonCheckbox, IonRouterLink } from '@ionic/react';
 import CheckboxItem from '../components/CheckboxItem';
 
 const Tab2: React.FC = () => {
   return (
     <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>
+            Calendar Page?
+          </IonTitle>
+        </IonToolbar>
+      </IonHeader>
       <IonContent className="ion-padding">
         <IonList>
-        <IonItem>
+          <IonItem>
             <IonCheckbox aria-label="Checkbox Label" />
           </IonItem>
           <IonItem>
@@ -22,4 +29,3 @@ const Tab2: React.FC = () => {
 };
 
 export default Tab2;
-
