b447487764027d4d930dc1ead300d21592534e31
Logan Gray <lgray@proton.me>
2024-01-31 11:45:35 -0800

    Setting work

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonItem, IonList } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 
@@ -7,16 +7,21 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Settings</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <IonList>
+          <IonItem>
+            <IonInput placeholder="Enter Name"></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonInput placeholder="Enter Input"></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonInput placeholder="Enter Input"></IonInput>
+          </IonItem>
+        </IonList>
       </IonContent>
     </IonPage>
   );
