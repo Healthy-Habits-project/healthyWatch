93f8c6ca55e7194dad606bb7176abb7c45a17d7c
Pupo-Professional <pupo.professional@gmail.com>
2024-02-01 16:02:48 -0800

    Calendar work

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,17 +1,38 @@
-// src/pages/CheckboxPage.tsx
-import React from 'react';
-import { IonContent, IonHeader, IonToolbar, IonPage, IonList, IonItem, IonCheckbox, IonRouterLink } from '@ionic/react';
-import FullCalendarComponent from '../components/FullCalendarComponent';
+import React, { useState } from 'react';
+import { IonDatetime, IonLabel, IonContent, IonPage, IonHeader, IonToolbar, IonTitle } from '@ionic/react';
 
-const  CalendarPage: React.FC = () => {
+function Example() {
+  const [selectedDate, setSelectedDate] = useState<string>('');
+
+  const handleDateChange = (event: CustomEvent) => {
+    // Assuming the selected date is in the format YYYY-MM-DDTHH:mm
+    const selectedDateTime = new Date(event.detail.value as string);
+    const formattedDate = selectedDateTime.toLocaleString('en-US', {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+    });
+    setSelectedDate(formattedDate);
+  };
+
   return (
     <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Date and Time Picker</IonTitle>
+        </IonToolbar>
+      </IonHeader>
       <IonContent className="ion-padding">
-        {/* Render the FullCalendarComponent */}
-        <FullCalendarComponent />
+        <IonLabel>Select a Date and Time:</IonLabel>
+        <IonDatetime
+          value={selectedDate}
+          onIonChange={handleDateChange}
+        />
       </IonContent>
     </IonPage>
   );
-};
+}
 
-export default CalendarPage;
+export default Example;
