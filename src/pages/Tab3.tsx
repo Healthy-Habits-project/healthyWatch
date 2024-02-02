6901cd72f9a33a2df321e88eb09c099f2d2911a4
Logan Gray <lgray@proton.me>
2024-02-02 10:35:45 -0800

    Settings save button progress

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,8 +1,15 @@
-import { IonContent, IonHeader, IonGrid, IonRow, IonButton, IonButtons, IonBackButton, IonPage, IonTitle, IonToolbar, IonInput, IonItem, IonList } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonGrid, IonRow, IonButton, IonPage, IonTitle, IonToolbar, IonInput, IonItem, IonList } from '@ionic/react';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
+  const [name, setName] = useState(''); // State to store the user's name input
+
+  const handleSave = () => {
+    // Here you can handle the save action, for example, logging the name or saving it elsewhere
+    console.log(name); // For demonstration, we'll just log the name to the console
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -13,11 +20,11 @@ const Tab3: React.FC = () => {
       <IonContent fullscreen>
         <IonList>
           <IonItem>
-            <IonInput placeholder="Enter Name"></IonInput>
+            <IonInput placeholder="Enter Name" value={name} onIonChange={e => setName(e.detail.value!)}></IonInput>
           </IonItem>
           <IonGrid>
             <IonRow>
-              <IonButton>Save</IonButton>
+              <IonButton onClick={handleSave}>Save</IonButton>
               <IonButton>Export</IonButton>
             </IonRow>
           </IonGrid>
