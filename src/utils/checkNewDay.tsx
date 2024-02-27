ac828a6a8c76928b437c8bdd552e9177744dc914
Default-damage <gabriel.augustus.pupo@gmail.com>
2024-02-26 18:02:03 -0800

    Refactor / CheckboxNewDay

--- src/utils/checkNewDay.tsx
+++ src/utils/checkNewDay.tsx
@@ -1,13 +1,16 @@
 // utils/checkNewDay.ts
 export const isNewDay = (): boolean => {
-    const today: string = new Date().toDateString();
-    const lastVisitDate: string | null = localStorage.getItem('lastVisitDate');
+    const simulatedToday: string = new Date('2024-02-28').toDateString(); // Set this to a date that is after the lastVisitDat
+const today: string = new Date().toDateString();
+const lastVisitDate: string | null = localStorage.getItem('lastVisitDate');
 
-    if (today !== lastVisitDate) {
-        localStorage.setItem('lastVisitDate', today);
-        return true; // It's a new day
-    }
+console.log('Today:', today, 'Last Visit:', lastVisitDate); // Add this line for debugging
 
-    return false; // Not a new day
+if (today !== lastVisitDate) {
+    localStorage.setItem('lastVisitDate', today);
+    return true; // It's a new day
+}
+
+return false; // Not a new day
 }
 export default isNewDay
