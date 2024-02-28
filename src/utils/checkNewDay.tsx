d74f6b47eab5e3177caffddc024e2506702ebbee
Pupo-Professional <pupo.professional@gmail.com>
2024-02-29 08:17:04 +1400

    Timezone Local

--- src/utils/checkNewDay.tsx
+++ src/utils/checkNewDay.tsx
@@ -1,16 +1,16 @@
 export const isNewDay = (pageKey: string): boolean => {
-  const today = new Date().toISOString().split('T')[0];
-  const lastVisitKey = `${pageKey}-lastVisitDate`;
-  const lastVisitDate = localStorage.getItem(lastVisitKey);
+    const today = new Date().toLocaleDateString('en-CA'); // Outputs YYYY-MM-DD in local time zone
+    const lastVisitKey = `${pageKey}-lastVisitDate`;
+    const lastVisitDate = localStorage.getItem(lastVisitKey);
 
-  console.log(`Today: ${today}, Last Visit: ${lastVisitDate}`);
+    console.log(`Today: ${today}, Last Visit: ${lastVisitDate}`);
 
-  if (today !== lastVisitDate) {
-    localStorage.setItem(lastVisitKey, today);
-    console.log(`It's a new day for ${pageKey}.`);
-    return true;
-  }
+    if (today !== lastVisitDate) {
+        localStorage.setItem(lastVisitKey, today);
+        console.log(`It's a new day for ${pageKey}.`);
+        return true;
+    }
 
-  console.log(`Not a new day for ${pageKey}.`);
-  return false;
+    console.log(`Not a new day for ${pageKey}.`);
+    return false;
 };
